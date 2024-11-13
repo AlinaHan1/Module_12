695d4318c1f3244927446f73cc8b3910d7dfec26
AlinaHan1 <alinakoshenko@gmail.com>
2024-11-13 22:44:55 +0300

    Add files via upload

--- module_12_2.py
+++ module_12_2.py
@@ -3,6 +3,9 @@ import unittest
 
 
 class TournamentTest(unittest.TestCase):
+    is_frozen = True
+    # @unittest.skipIf(is_frozen == True, тест не проходит. is_frozen == False, тест проходит)
+
     @classmethod
     def setUpClass(cls):
         cls.all_results = []
@@ -12,21 +15,21 @@ class TournamentTest(unittest.TestCase):
         self.run_2 = Runner('Андрей', 9)
         self.run_3 = Runner('Ник', 3)
 
-
-
+    @unittest.skipIf(True, 'Тесты в этом кейсе заморожены')
     def test_1(self):
         tournament1 = Tournament(90, self.run_1, self.run_3)
         result = tournament1.start()
         TournamentTest.all_results.append(result)
         self.assertTrue(result[2] == 'Ник')
 
-
+    @unittest.skipIf(True, 'Тесты в этом кейсе заморожены')
     def test_2(self):
         tournament2 = Tournament(90, self.run_2, self.run_3)
         result = tournament2.start()
         TournamentTest.all_results.append(result)
         self.assertTrue(result[2] == 'Ник')
 
+    @unittest.skipIf(True, 'Тесты в этом кейсе заморожены')
     def test_3(self):
         tournament3 = Tournament(90, self.run_1, self.run_2, self.run_3)
         result = tournament3.start()
